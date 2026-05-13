1efb4c153c302a8c99029c06f033080615534fce
dlcsinfo-gif <dlcsinfo@itech-india.org>
2026-05-13 20:34:54 +0530

    Create layout.js

---
+++ layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "HIV Guidelines Chatbot",
+  description: "Secure HIV Treatment Guidelines Assistant"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
